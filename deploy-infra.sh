194d9b2cfb9471a6ca10c865e759ece526cf278d
Josh Pschorr <josh@pschorr.codes>
2019-12-17 11:11:43 -0800

    Remove explicitly created EC2 instances

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -50,8 +50,5 @@ aws cloudformation deploy \
 if [ $? -eq 0 ]; then
   aws cloudformation list-exports \
     --profile awsbootstrap \
-    --query "Exports[?starts_with(Name,'InstanceEndpoint')].Value"
-  aws cloudformation list-exports \
-    --profile awsbootstrap \
     --query "Exports[?starts_with(Name,'LBEndpoint')].Value"
 fi
