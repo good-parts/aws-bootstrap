4538c2990a682e529a257ec200546abede667e5f
Josh Pschorr <josh@pschorr.codes>
2019-12-19 19:13:06 -0800

    Add https listener to service

--- server.js
+++ server.js
@@ -1,8 +1,30 @@
 const { hostname } = require('os');
 const http = require('http');
+const https = require('https');
+const fs = require('fs');
+
 const STACK_NAME = process.env.STACK_NAME || "Unknown Stack";
-const message = `Hello World from ${hostname()} in ${STACK_NAME}\n`;
 const port = 8080;
+const httpsPort = 8443;
+const httpsKey = '../keys/key.pem'
+const httpsCert = '../keys/cert.pem'
+
+if (fs.existsSync(httpsKey) && fs.existsSync(httpsCert)) {
+    console.log('Starting https server')
+    const message = `Hello HTTPS World from ${hostname()} in ${STACK_NAME}\n`;
+    const options = { key: fs.readFileSync(httpsKey), cert: fs.readFileSync(httpsCert) };
+    const server = https.createServer(options, (req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end(message);
+    });
+    server.listen(httpsPort, hostname, () => {
+        console.log(`Server running at http://${hostname()}:${httpsPort}/`);
+    });
+}
+
+console.log('Starting http server')
+const message = `Hello HTTP World from ${hostname()} in ${STACK_NAME}\n`;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
@@ -11,3 +33,4 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname()}:${port}/`);
 });
+
