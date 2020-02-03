5250a7e610ca3d37431b6f253d1dc2a326c7092e
Josh Pschorr <josh@pschorr.codes>
2020-02-03 06:55:02 -0800

    Add hostname to hello world message

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 const { hostname } = require('os');
 const http = require('http');
-
-const message = 'Hello Cloud\n';
+const message = `Hello World from ${hostname()}\n`;
 const port = 8080;
-
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end(message);
 });
-
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname()}:${port}/`);
 });
