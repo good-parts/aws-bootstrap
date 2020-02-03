84937e8f0c955d1fd580fe0ce876327d1d43a801
Josh Pschorr <josh@pschorr.codes>
2020-02-03 06:47:04 -0800

    Change Hello World to Hello Cloud

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const { hostname } = require('os');
 const http = require('http');
 
-const message = 'Hello World\n';
+const message = 'Hello Cloud\n';
 const port = 8080;
 
 const server = http.createServer((req, res) => {
